a52c494dcfaac2a58f7e2d2d8d979fcc871d2936
CodeGonshik <nikolaysimson20@gmail.com>
2026-08-15 00:05:34 +0300

    refactor: cmake optimization

--- Light/cmake/lightlibConfig.cmake
+++ Light/cmake/lightlibConfig.cmake
@@ -1,9 +1,8 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(Boost CONFIG REQUIRED COMPONENTS beast asio filesystem system thread date_time smart_ptr optional variant algorithm)
+find_dependency(Boost CONFIG REQUIRED COMPONENTS beast asio system thread filesystem)
 find_dependency(fmt CONFIG REQUIRED)
 find_dependency(nlohmann_json CONFIG REQUIRED)
-find_dependency(jwt-cpp CONFIG REQUIRED)
 find_dependency(hiredis CONFIG REQUIRED)
 find_dependency(ZLIB REQUIRED)
 find_dependency(PostgreSQL REQUIRED)
